eec2256524658573ad35cb4c2fe183babf9b2917
taypsl <tayparsell@gmail.com>
2017-04-04 18:48:58 -0700

    created volunteer components and updated index.js

--- js/index.js
+++ js/index.js
@@ -2,29 +2,40 @@ require('babel-polyfill');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, Route, IndexRoute, hashHistory} from 'react-router';
-
+import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
 import Layout from './components/layout';
 import YouthSurvey from './components/youth-survey';
 // make these components-->
 import VolunteerSurvey from './components/volunteer-survey';
 import Volunteers from './components/volunteers';
 
-const routes = (
-	<Router history={hashHistory}>
-		<Route path="/" component={Layout}>
-			<Route path=":participate" component={YouthSurvey} />
-			<Route path=":volunteer" component={VolunteerSurvey} />
-			<Route path=":our-volunteers" component={Volunteers} />
-		</Route>
-	</Router>
-)
+// const routes = (
+// )
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router history={hashHistory}>
+        <Route path="/" component={Layout}>
+          <Route path="/participate" component={YouthSurvey} />
+
+        </Route>
+      </Router>
+    )
+  }
+}
+
+//          <Route path=":volunteer" component={VolunteerSurvey} />
+//          <Route path=":our-volunteers" component={Volunteers} />
+
+const Home = () => <h1>Hello from Home!</h1>
+const Address = () => <h1>We are located at 555 Jackson St.</h1>
 
 document.addEventListener('DOMContentLoaded', () =>
-	ReactDOM.render(routes, document.getElementById('app'))
+	ReactDOM.render(<App />, document.getElementById('app'))
 );
 
-// layout should render 
+// layout should render
 	// nav
 	// get advice
 	// meet volunteers
@@ -36,4 +47,3 @@ document.addEventListener('DOMContentLoaded', () =>
 		// button on click renders all volunteer cards page
 
 // participation buttons should render survey page
-
