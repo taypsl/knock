3fa75aed081ffed9fa926f8bf741812cf1fe6271
taypsl <tayparsell@gmail.com>
2017-04-04 18:44:41 -0700

    updating server models

--- server.js
+++ server.js
@@ -14,106 +14,115 @@ app.use(bodyParser.json());
 mongoose.Promise = global.Promise;
 
 app.get('/users', (req, res) => {
-  User
-    .find()
-    .exec()
-    .then(users => {
-      res.json(users.map(user => user.apiRepr()));
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went terribly wrong'});
-    });
+	User
+	.find()
+	.exec()
+	.then(users => {
+		res.json(users.map(user => user.apiRepr()));
+	})
+	.catch(err => {
+		console.error(err);
+		res.status(500).json({error: 'something went terribly wrong'});
+	});
 });
 
 app.get('/users/:id', (req, res) => {
-  User
-    .findById(req.params.id)
-    .exec()
-    .then(user => res.json(user.apiRepr()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went horribly awry'});
-    });
+	User
+	.findById(req.params.id)
+	.exec()
+	.then(user => res.json(user.apiRepr()))
+	.catch(err => {
+		console.error(err);
+		res.status(500).json({error: 'something went horribly awry'});
+	});
 });
 
 app.post('/users', (req, res) => {
-  console.log(req.body)
-  const requiredFields = ['title', 'content', 'author'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
+	console.log(req.body)
+	const requiredFields = ['username', 'phone', 'email', 'background'];
+	for (let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
 
-  User
-    .create({
-      title: req.body.title,
-      content: req.body.content,
-      author: req.body.author
-    })
-    .then(blogUser => res.status(201).json(blogUser.apiRepr()))
-    .catch(err => {
-        console.error(err);
-        res.status(500).json({error: 'Something went wrong'});
-    });
+	User
+	.create({
+		username: req.body.username,
+		phone: req.body.phone,
+		email: req.body.email,
+		background: req.body.background,
+		mentor: req.body.mentor,
+		preferences: req.body.preferences,
+		education: req.body.education,
+		children: req.body.children,
+		ethnicity: req.body.ethnicity,
+		dems: req.body.dems,
+		title: req.body.title,
+		website: req.body.website
+	})
+	.then(user => res.status(201).json(user.apiRepr()))
+	.catch(err => {
+		console.error(err);
+		res.status(500).json({error: 'Something went wrong'});
+	});
 
 });
 
 
 app.delete('/users/:id', (req, res) => {
-  User
-    .findByIdAndRemove(req.params.id)
-    .exec()
-    .then(() => {
-      res.status(204).json({message: 'success'});
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went terribly wrong'});
-    });
+	User
+	.findByIdAndRemove(req.params.id)
+	.exec()
+	.then(() => {
+		res.status(204).json({message: 'success'});
+	})
+	.catch(err => {
+		console.error(err);
+		res.status(500).json({error: 'something went terribly wrong'});
+	});
 });
 
 
 app.put('/users/:id', (req, res) => {
-  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    res.status(400).json({
-      error: 'Request path id and request body id values must match'
-    });
-  }
+	if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+		res.status(400).json({
+			error: 'Request path id and request body id values must match'
+		});
+	}
 
-  const updated = {};
-  const updateableFields = ['title', 'content', 'author'];
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      updated[field] = req.body[field];
-    }
-  });
+	const updated = {};
+	const updateableFields = ['title', 'content', 'author'];
+	updateableFields.forEach(field => {
+		if (field in req.body) {
+			updated[field] = req.body[field];
+		}
+	});
 
-  User
-    .findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
-    .exec()
-    .then(updatedUser => res.status(201).json(updatedUser.apiRepr()))
-    .catch(err => res.status(500).json({message: 'Something went wrong'}));
+	User
+	.findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
+	.exec()
+	.then(updatedUser => res.status(201).json(updatedUser.apiRepr()))
+	.catch(err => res.status(500).json({message: 'Something went wrong'}));
 });
 
 
 app.delete('/:id', (req, res) => {
-  Users
-    .findByIdAndRemove(req.params.id)
-    .exec()
-    .then(() => {
-      console.log(`Deleted blog user with id \`${req.params.ID}\``);
-      res.status(204).end();
-    });
+	User
+	.findByIdAndRemove(req.params.id)
+	.exec()
+	.then(() => {
+		console.log(`Deleted user with id \`${req.params.ID}\``);
+		res.status(204).end();
+	});
 });
 
 
 app.use('*', function(req, res) {
-  res.status(404).json({message: 'Not Found'});
+	res.status(404).json({message: 'Not Found'});
 });
 
 // closeServer needs access to a server object, but that only
@@ -123,43 +132,43 @@ let server;
 
 // this function connects to our database, then starts the server
 function runServer(databaseUrl=DATABASE_URL, port=PORT) {
-  return new Promise((resolve, reject) => {
-    mongoose.connect(databaseUrl, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port}`);
-        resolve();
-      })
-      .on('error', err => {
-        mongoose.disconnect();
-        reject(err);
-      });
-    });
-  });
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, err => {
+			if (err) {
+				return reject(err);
+			}
+			server = app.listen(port, () => {
+				console.log(`Your app is listening on port ${port}`);
+				resolve();
+			})
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err);
+			});
+		});
+	});
 }
 
 // this function closes the server, and returns a promise. we'll
 // use it in our integration tests later.
 function closeServer() {
-  return mongoose.disconnect().then(() => {
-     return new Promise((resolve, reject) => {
-       console.log('Closing server');
-       server.close(err => {
-           if (err) {
-               return reject(err);
-           }
-           resolve();
-       });
-     });
-  });
+	return mongoose.disconnect().then(() => {
+	 return new Promise((resolve, reject) => {
+		 console.log('Closing server');
+		 server.close(err => {
+			 if (err) {
+				 return reject(err);
+			 }
+			 resolve();
+		 });
+	 });
+ });
 }
 
 // if server.js is called directly (aka, with `node server.js`), this block
 // runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
+	runServer().catch(err => console.error(err));
 };
 
 module.exports = {runServer, app, closeServer};
