6592efe5000a221441df1bd8fb3ade831076c31e
taypsl <tayparsell@gmail.com>
2017-04-07 17:23:53 -0700

    updated css, added google fonts cdn

--- js/components/how-it-works.js
+++ js/components/how-it-works.js
@@ -5,11 +5,45 @@ export default class HowItWorks extends React.Component {
 	render() {
 		return (
 			<div className="how-it-works" id="link1">
+				<div className="grey-bar"></div>
 				<section>
-					<h2>HOW IT WORKS</h2>
-					<p>Meet real people who work in professional careers and learn about what they do. Whether you're interested in marketing, technology, science, or art, there's someone out there who can share their experiences with you.</p>
-					<div className="links survey-link"><Link to={'/participate'}>Get Started</Link></div>
+					<h2 className="section-header">how it works</h2>
+					<div className="container list-item">
+						<div className="row">
+							<div className="col-sm-2">
+								<img src="../assets/images/icn_list1@2x.png" />
+							</div>
+							<div className="col-sm-10">
+								<p className="list-text">Meet real people who work in professional careers and learn about what they do.</p>
+							</div>
+						</div>
+					</div>
+					
+					<div className="container list-item">
+						<div className="row">
+							<div className="col-sm-2">
+								<img src="../assets/images/icn_list2@2x.png" />
+							</div>
+							<div className="col-sm-10">
+								<p className="list-text">Set up a time to have a 20 minute call where you can ask questions about their experiences in their daily job, and how they achieved their career goals.</p>
+							</div>
+						</div>
+					</div>	
+
+					<div className="container list-item">
+						<div className="row">
+							<div className="col-sm-2">
+								<img src="../assets/images/icn_list3@2x.png" />
+							</div>
+							<div className="col-sm-10">
+								<p className="list-text">Check out some of our volunteers below, and fill out a short questionaire to get started.</p>
+							</div>
+						</div>
+					</div>
+
+					<Link to={'/participate'}><div className="btn-main">get started</div></Link>
 				</section>
+				<div className="grey-bar"></div>
 			</div>
 		)
 	}
