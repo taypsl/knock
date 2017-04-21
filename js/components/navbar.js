68d4d321ea5766309378c4cd51409dcfa030e724
taypsl <tayparsell@gmail.com>
2017-04-21 01:26:30 -0700

    adding responsive dropdiwn menu, currently lists aren't disappearing on small screen size

--- js/components/navbar.js
+++ js/components/navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
 // import {HashLink as Link} from 'react-router-hash-link';
-
 // may need to import hashLink===>
 // npm install --save react-router-hash-link and import {HashLink as Link} from react-router-hash-link
 
@@ -11,10 +10,10 @@ export default class Navbar extends React.Component {
 			<div className="header">
 				<h1 className="brand"><Link to={"/"}>KNOCK</Link></h1>
 				<input id="menu-toggle" type="checkbox" />
-				<label className='menu-button-container' htmlFor="menu-toggle">
-					<div className='menu-button'></div>
+				<label className="menu-button-container" htmlFor="menu-toggle">
+					<div className="menu-button"></div>
   				</label>
-				<ul id="dropdown" className="dropdown">
+				<ul id="dropdown" className="menu">
 					<li className="links"><Link to={"/#howitworks"}>HOW IT WORKS</Link></li>
 					<li className="links"><Link to={"/#meetamentor"}>MEET A MENTOR</Link></li>
 					<li className="links"><Link to={"/#getinvolved"}>GET INVOLVED</Link></li>
